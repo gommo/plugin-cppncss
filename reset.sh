863f4f44127d24c569c13df5aff7314c93aafe9b
liangjichen <liangjichen@dev.java.net>
2010-07-14 09:20:45 +0000

    remove blank characters by LiangjiChen, only for testing commit

--- reset.sh
+++ reset.sh
@@ -7,5 +7,5 @@ rm -Rf work/jobs/*/workspace/*/target
 rm -Rf work/jobs/*/workspace/dist
 rm -Rf work/jobs/*/workspace/build
 rm -Rf work/fingerprints
-rm -Rf work/queue.txt 
-rm -Rf work/secret.key 
+rm -Rf work/queue.txt
+rm -Rf work/secret.key
